cb2e810f802d6e68441881ba9ad5016fcb539eb6
ReVaN <revannn5480@gmail.com>
2019-07-10 20:17:08 +0200

    First part of task

--- ListWithDeleteButton/App.js
+++ ListWithDeleteButton/App.js
@@ -1,9 +1,47 @@
+const Person = (props) =>  {
+  return (
+    <li>
+      <span>{props.name}</span>
+      <button>Usuń</button>
+    </li>
+  )
+}
+
 class PeopleList extends React.Component {
+
+  state = {
+    people: [
+      {
+        id: 1,
+        name: 'Asia'
+      },
+      {
+        id: 2,
+        name: 'Ania'
+      },
+      {
+        id: 3,
+        name: 'Basia'
+      },
+      {
+        id: 4,
+        name: 'Jolka'
+      }
+    ]
+  }
+
+  handleDelete = () => {
+    console.log('Usuwanie');
+  }
 
   render() {
     return(
       <>
-      
+        <ul>
+          {this.state.people.map(person => 
+            <Person key={person.id} name={person.name} delete={this.handleDelete}/>)
+          }
+        </ul>
       </>
     )
   }
