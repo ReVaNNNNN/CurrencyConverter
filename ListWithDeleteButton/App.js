9c363725e1f204c3cad3165cf047d86e8c1d17c2
ReVaN <revannn5480@gmail.com>
2019-07-10 20:05:58 +0200

    Start of new little project

---
+++ ListWithDeleteButton/App.js
@@ -0,0 +1,12 @@
+class PeopleList extends React.Component {
+
+  render() {
+    return(
+      <>
+      
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<PeopleList />, document.getElementById('root'))
