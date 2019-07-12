af59f0a406626f7f397a54d697a43fb9e563f99e
ReVaN <revannn5480@gmail.com>
2019-07-12 07:17:25 +0200

    Counter - simple implementation

--- Counter/App.js
+++ Counter/App.js
@@ -1,8 +1,29 @@
 class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  increment = () => {
+    this.setState(prevState => ({
+      counter: ++prevState.counter
+    }))
+  }
+
+  decrement = () => {
+    this.setState(prevState => ({
+      counter: --prevState.counter
+    }))
+  }
 
   render() {
     return(
       <>
+        <p>{this.state.counter}</p>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
       </>
     )
   }
