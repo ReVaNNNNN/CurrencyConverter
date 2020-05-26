9836b9223ad5b44d301830601705a3865bfaf934
ReVaN <revannn5480@gmail.com>
2020-05-26 13:59:21 +0200

    Finish Exercise

--- Exercises/App.js
+++ Exercises/App.js
@@ -1,2 +1,36 @@
+const PositiveMessage = () => <p> Możesz obejrzeć film. Zapraszamy !</p>;
 
+const NegativeMessage = () => (
+  <p> Nie możesz obejrzeć tego filmu jeśli masz mniej niż 16 lat!</p>
+);
 
+class App extends React.Component {
+  state = {
+    isConfirmed: false,
+  };
+
+  handleCheckboxChange = () => {
+    this.setState((prevState) => ({
+      isConfirmed: !prevState.isConfirmed,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Kup blet na horror roku!</h1>
+        <label>
+          <input
+            type="checkbox"
+            onChange={this.handleCheckboxChange}
+            checked={this.state.isConfirmed}
+          />{" "}
+          Mam co najmniej 16 lat
+        </label>
+        {this.state.isConfirmed ? <PositiveMessage /> : <NegativeMessage />}
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
