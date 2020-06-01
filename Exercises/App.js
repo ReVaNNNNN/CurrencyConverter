ca9c63bb98b33e59013ecc8a68b7f2890f909194
ReVaN <revannn5480@gmail.com>
2020-06-01 14:20:10 +0200

    Destructurization Exercise - start

--- Exercises/App.js
+++ Exercises/App.js
@@ -1,50 +1,9 @@
-class App extends React.Component {
-  state = {
-    availableProducts: 7,
-    shoppingCart: 0,
-  };
-
-  handleRemoveFromCart = () => {
-    this.setState({
-      shoppingCart: this.state.shoppingCart - 1,
-    });
-  };
-
-  handleAddFromCart = () => {
-    this.setState({
-      shoppingCart: this.state.shoppingCart + 1,
-    });
-  };
-
-  handleBuy = () => {
-    this.setState({
-      availableProducts: this.state.availableProducts - this.state.shoppingCart,
-      shoppingCart: 0,
-    });
-  };
+class ListItems extends React.Component {
+  state = {};
 
   render() {
-    const { shoppingCart, availableProducts } = this.state;
-    const style = shoppingCart === 0 ? { opacity: 0.3 } : {};
-    return (
-      <>
-        <button
-          disabled={shoppingCart ? false : true}
-          onClick={this.handleRemoveFromCart}
-        >
-          -
-        </button>
-        <span style={style}>{shoppingCart}</span>
-        <button
-          disabled={shoppingCart >= availableProducts ? true : false}
-          onClick={this.handleAddFromCart}
-        >
-          +
-        </button>
-        {shoppingCart > 0 && <button onClick={this.handleBuy}>KUP</button>}
-      </>
-    );
+    return <></>;
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ListItems />, document.getElementById("root"));
