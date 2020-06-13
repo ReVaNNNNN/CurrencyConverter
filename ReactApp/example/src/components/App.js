e86ec02687a410a54f8cf6f6d36432336d4b6bd8
ReVaN <revannn5480@gmail.com>
2020-06-13 14:15:39 +0200

    CreateReact App - stopper

--- ReactApp/example/src/components/App.js
+++ ReactApp/example/src/components/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
+import SwitchButton from "./SwitchButton";
 
 class App extends Component {
+  state = {
+    time: 0,
+    active: false,
+  };
+
+  handleClick = () => {
+    if (this.state.active) {
+      clearInterval(this.intervalId);
+    } else {
+      this.intervalId = setInterval(() => this.addSecond(), 1000);
+    }
+
+    this.setState({
+      active: !this.state.active,
+    });
+  };
+
+  addSecond = () => {
+    this.setState({
+      time: this.state.time + 1,
+    });
+  };
   render() {
-    return <div className="App">Aplikacja</div>;
+    return (
+      <>
+        <p>{this.state.time}</p>
+        <SwitchButton click={this.handleClick} active={this.state.active} />
+      </>
+    );
   }
 }
 
