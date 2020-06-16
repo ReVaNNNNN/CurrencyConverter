87d5b8b03fd41bf091d5d52319d692a56ea1c47d
ReVaN <revannn5480@gmail.com>
2020-06-16 15:05:25 +0200

    React + Ajax - exercise start

--- ReactApp/example/src/components/App.js
+++ ReactApp/example/src/components/App.js
@@ -1,29 +1,10 @@
 import React, { Component } from "react";
-import Word from "./Word";
 
 class App extends Component {
-  state = {
-    words: [],
-    isLoaded: false,
-  };
-
-  componentDidMount() {
-    fetch("data/words.json")
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          words: data.words,
-          isLoaded: true,
-        });
-      });
-  }
+  state = {};
 
   render() {
-    console.log(new Date().getTime());
-    const words = this.state.words.map((word) => (
-      <Word key={word.id} en={word.en} pl={word.pl} />
-    ));
-    return <ul>{this.state.isLoaded ? words : "Wczytuję dane"}</ul>;
+    return;
   }
 }
 
