940eb4aefccea6502f27f9fc8ca18599643433f9
ReVaN <revannn5480@gmail.com>
2020-06-16 15:22:22 +0200

    React  + Ajax exercise finihs

--- ReactApp/example/src/components/App.js
+++ ReactApp/example/src/components/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  state = {};
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const users = JSON.parse(xhr.response);
+        this.setState({
+          users,
+        });
+      }
+    };
+    xhr.send();
+  }
 
   render() {
-    return;
+    const users = this.state.users.map((user) => (
+      <div key={user.id}>
+        <h4>{user.name}</h4>
+        <p>{user.address.city}</p>
+      </div>
+    ));
+    return <>{users}</>;
   }
 }
 
