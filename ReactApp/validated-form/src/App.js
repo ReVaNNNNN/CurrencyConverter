04515d082b95fa3ec7e55ede4c4b40f79b21cba7
ReVaN <revannn5480@gmail.com>
2020-06-20 09:52:48 +0200

    React App - validation

--- ReactApp/validated-form/src/App.js
+++ ReactApp/validated-form/src/App.js
@@ -6,7 +6,22 @@ class App extends Component {
     username: "",
     email: "",
     pass: "",
-    accept: "",
+    accept: false,
+
+    errors: {
+      username: false,
+      email: false,
+      pass: false,
+      accept: false,
+    },
+  };
+
+  messages = {
+    username_incorrect:
+      "Nazwa musi byc dłuższa niż 10 znaków i nie może zawierać spacji",
+    email_incorrect: "Brak @ w emailu",
+    password_incorrect: "Hasło musi mieć 8 znaków",
+    accept_incorrect: "Nie potwierdzono zgody",
   };
 
   handleChange = (e) => {
@@ -27,7 +42,86 @@ class App extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const validation = this.formValidation();
+
+    if (validation.correct) {
+      this.setState({
+        username: "",
+        email: "",
+        pass: "",
+        accept: false,
+        message: "Formularz został wysłany",
+
+        errors: {
+          username: false,
+          email: false,
+          pass: false,
+          accept: false,
+        },
+      });
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          email: !validation.email,
+          pass: !validation.password,
+          accept: !validation.accept,
+        },
+      });
+    }
   };
+
+  formValidation = () => {
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let correct = false;
+
+    if (
+      this.state.username.length >= 10 &&
+      this.state.username.indexOf(" ") === -1
+    ) {
+      username = true;
+    }
+
+    if (this.state.email.indexOf("@") !== -1) {
+      email = true;
+    }
+
+    if (this.state.pass.length === 8) {
+      password = true;
+    }
+
+    if (this.state.accept) {
+      accept = true;
+    }
+
+    if (username && email && password && accept) {
+      correct = true;
+    }
+
+    return {
+      correct,
+      username,
+      email,
+      password,
+      accept,
+    };
+  };
+
+  componentDidUpdate() {
+    if (this.state.message !== "") {
+      setTimeout(
+        () =>
+          this.setState({
+            message: "",
+          }),
+        3000
+      );
+    }
+  }
 
   render() {
     return (
@@ -42,6 +136,9 @@ class App extends Component {
               value={this.state.username}
               onChange={this.handleChange}
             />
+            {this.state.errors.username && (
+              <span>{this.messages.username_incorrect}</span>
+            )}
           </label>
 
           <label htmlFor="email">
@@ -53,6 +150,9 @@ class App extends Component {
               value={this.state.email}
               onChange={this.handleChange}
             />
+            {this.state.errors.email && (
+              <span>{this.messages.email_incorrect}</span>
+            )}
           </label>
 
           <label htmlFor="password">
@@ -62,8 +162,11 @@ class App extends Component {
               type="password"
               name="pass"
               value={this.state.pass}
-              onChange={this.pass}
+              onChange={this.handleChange}
             />
+            {this.state.errors.pass && (
+              <span>{this.messages.password_incorrect}</span>
+            )}
           </label>
 
           <label htmlFor="accept">
@@ -73,12 +176,15 @@ class App extends Component {
               name="accept"
               checked={this.state.accept}
               onChange={this.handleChange}
-            />{" "}
+            />
             Wyrażam zgodę
           </label>
-
+          {this.state.errors.accept && (
+            <span>{this.messages.accept_incorrect}</span>
+          )}
           <button>Zapisz się</button>
         </form>
+        {this.state.message && <h3>{this.state.message}</h3>}
       </div>
     );
   }
