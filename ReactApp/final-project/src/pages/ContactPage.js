de5d4a6aa1c70866f0b52316d5a6afabf6063142
ReVaN <revannn5480@gmail.com>
2020-07-02 15:39:15 +0200

    Final project - contact page

--- ReactApp/final-project/src/pages/ContactPage.js
+++ ReactApp/final-project/src/pages/ContactPage.js
@@ -1,7 +1,55 @@
 import React from "react";
+import "../styles/ContactPage.css";
+import { Prompt } from "react-router-dom";
 
-const ContactPage = () => {
-  return <div>Strona startowa</div>;
-};
+class ContactPage extends React.Component {
+  state = {
+    value: "",
+    isEmpty: true,
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      value: "",
+      isEmpty: true,
+    });
+  };
+
+  handleChange = (e) => {
+    if (e.target.value.length > 0) {
+      this.setState({
+        value: e.target.value,
+        isEmpty: false,
+      });
+    } else {
+      this.setState({
+        value: e.target.value,
+        isEmpty: true,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Wpisz wiadomość..."
+          ></textarea>
+          <button>Wyślij</button>
+        </form>
+        <Prompt
+          when={!this.state.isEmpty}
+          message="Formularz nie został przesłany, czy na pewno chcesz przejść dalek ?"
+        />
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
--- ReactApp/final-project/src/pages/HomePage.js
+++ ReactApp/final-project/src/pages/HomePage.js
@@ -1,7 +1,30 @@
 import React from "react";
+import Article from "../components/Article";
+
+const articles = [
+  {
+    id: 1,
+    title: "Co by tu wymyślić ?",
+    text: "Ile dałbym by zapoznac cie wszystkie chwiel to co nie co nie",
+  },
+  {
+    id: 2,
+    title: "Myślę więc jestem, czy nie ?",
+    text: "Ile dałbym by zapoznac cie wszystkie chwiel to co nie co nie",
+  },
+  {
+    id: 3,
+    title: "Możesz wszystko, ale czy chesz ?",
+    text: "Ile dałbym by zapoznac cie wszystkie chwiel to co nie co nie",
+  },
+];
 
 const HomePage = () => {
-  return <div>Strona startowa</div>;
+  const artList = articles.map((article) => (
+    <Article key={article.id} {...article} />
+  ));
+
+  return <div>{artList}</div>;
 };
 
 export default HomePage;
